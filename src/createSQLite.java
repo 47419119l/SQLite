1eb61b27fd74dee322a5835b12d23504112d2c45
Sandra <47419119l@iespoblenou.org>
2015-11-25 17:58:01 +0100

    CreateSQLite Funciona!

---
+++ src/createSQLite.java
@@ -0,0 +1,51 @@
+/**
+ * Created by 47419119l on 25/11/15.
+ */
+import java.sql.*;
+
+public class createSQLite {
+
+    public static void main(String[] args) {
+        {
+            Connection c = null;
+            Statement stmt = null;
+
+            try {
+                Class.forName("org.sqlite.JDBC");
+                c = DriverManager.getConnection("jdbc:sqlite:theMoviesDBproject.db");
+                System.out.println("La BBDD ha sigut creada");
+
+                stmt = c.createStatement();
+                /*
+                    Creo les taules i les executo.
+                 */
+                String sql_movies = "CREATE TABLE MOVIES " +
+                        "(ID INT PRIMARY KEY     NOT NULL," +
+                        " ORIGINAL_TITLE TEXT    NOT NULL, " +
+                        " RELEASE_DATE   TEXT    NOT NULL)";
+
+                stmt.executeUpdate(sql_movies);
+
+                String sql_actor = "CREATE TABLE ACTORS " +
+                        "(ID_PELICULA    INT    NOT NULL," +
+                        " ID   INT    NOT NULL,"+
+                        " NAME TEXT    NOT NULL, " +
+                        " CHARACTER   TEXT    NOT NULL," +
+                        "PRIMARY KEY(ID_PELICULA, ID))";
+
+                stmt.executeUpdate(sql_actor);
+
+                stmt.close();
+                c.close();
+
+            } catch ( Exception e ) {
+                System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                System.exit(0);
+            }
+            System.out.println("Taules creades correctament.");
+        }
+
+    }
+
+
+}
--- src/theMoviesDBproject.java
+++ src/theMoviesDBproject.java
@@ -10,12 +10,14 @@ import javax.xml.transform.Transformer;
 import java.io.*;
 import java.net.*;
 
-/**
- * Created by dremon on 09/11/15.
- */
 class theMovieDBproject {
-
-
+    static String api_key="eec33652afa70e666fc6d094216e0714";
+    /**
+     * Extreu el HTML
+     * @param urlToRead
+     * @return
+     * @throws Exception
+     */
     public static String getHTML(String urlToRead) throws Exception {
         StringBuilder result = new StringBuilder();
         URL url = new URL(urlToRead);
@@ -32,32 +34,47 @@ class theMovieDBproject {
 
     public static void main(String[] args){
         String s = "";
-        String api_key = "";
 
         for (int i = 0; i < 40; i++) {
             int peli = 600 +i;
             String film = String.valueOf(peli);
-            String peticio = "https://api.themoviedb.org/3/movie/"+film+"?api_key="+api_key;
+            /*
+             Aquesta URL és per pelicules.
+             */
+            String peticioPeliculas = "https://api.themoviedb.org/3/movie/"+film+"?api_key="+api_key;
+            String peticioActors="https://api.themoviedb.org/3/movie/"+film+"/casts?api_key="+api_key;
             try {
-                s = getHTML(peticio);
+                s = getHTML(peticioPeliculas);
                 SJS(s);
+                System.out.println("-----------------------");
+                s=getHTML(peticioActors);
+                SJC(s);
             } catch (Exception e) {
-                System.out.println("La peli "+film+" no existeix");
+
             }
         }
 
 
     }
 
+    /**
+     * Funció per agafar pel·licules
+     * @param cadena
+     */
     public static void SJS (String cadena){
 
         Object obj02 =JSONValue.parse(cadena);
         JSONObject arra02=(JSONObject)obj02;
-        System.out.println(arra02.get("original_title"));
-        System.out.println(arra02.get("release_date"));
+        System.out.println("ID pel·licula : "+arra02.get("id"));
+        System.out.println("Titul original pel·licula : "+arra02.get("original_title"));
+        System.out.println("Data d'estrena : "+arra02.get("release_date"));
 
     }
 
+    /**
+     * Funció per agafar actors.
+     * @param cadena
+     */
     public static void SJC (String cadena){
         Object obj02 =JSONValue.parse(cadena);
         JSONObject arra02=(JSONObject)obj02;
@@ -66,7 +83,7 @@ class theMovieDBproject {
         for (int i = 0; i < arra03.size(); i++) {
 
             JSONObject jb= (JSONObject)arra03.get(i);
-            System.out.println(jb.get("character")+"<-->"+jb.get("name"));
+            System.out.println(jb.get("character")+"<-->"+jb.get("name")+"<--->"+jb.get("cast_id"));
 
         }
 
